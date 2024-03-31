f205ac068f2b52c650924d3efb7b63684f4692dd
Aisiqu <kxdex@mail.ru>
2024-04-01 01:09:20 +0300

    китай

---
+++ 713. Subarray Product Less Than K/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SubArray
+{
+    public class Solution
+    {
+        public int NumSubarrayProductLessThanK(int[] nums, int k)
+        {
+            int count = 0;
+            int elem_count = 2;
+
+            var arr = nums.Split(elem_count);
+            
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] nums = new int[10];
+            foreach (int i in nums)
+            {
+                nums[i] = rand.Next(0, 11);
+            }
+            Console.WriteLine();
+            Solution solution = new Solution();
+            Console.WriteLine("Лютое значение: {0}", solution.NumSubarrayProductLessThanK(nums, 100));
+        }
+    }
+
+}
